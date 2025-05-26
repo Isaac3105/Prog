d69fc582fd323bca24f84e76370f5933f0e245b5
Isaac3105 <isaac.absantos@gmail.com>
2025-05-26 10:37:51 +0100

    Did another exercise of the playground7

--- py07/replace.cpp
+++ py07/replace.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <sstream>
 #include "print.h" 
@@ -11,26 +12,16 @@ std::string tomenor(std::string a){
 }
 
 void replace_word(const std::string& fname, const std::string& old_word, const std::string& new_word, const std::string& out_fname){
-    std::ifstream arq(fname);
-    std::ofstream arq2(out_fname);
-
-    std::string line;
-    while (std::getline(arq, line)) {
-        std::istringstream iss(line);
-        std::string pal;
-        std::string c = "";
-
-        while (iss >> pal) {
-            if (tomenor(pal) == tomenor(old_word)) {
-                arq2 << c << new_word;
-            } else {
-                arq2 << c << pal;
-            }
-            c = " ";
+    
+    std::ifstream file(fname);
+    std::ofstream file2(out_fname);
+    std::string atual;
+    while(std::getline(file,atual)){
+        while(tomenor(atual).find(tomenor(old_word)) != std::string::npos){
+            size_t index = tomenor(atual).find(tomenor(old_word));
+            atual = atual.replace(index, old_word.length(),new_word);
         }
-        arq2 << '\n';
+        file2 << atual << '\n';
     }
-
-    arq.close();
-    arq2.close();
+    
 }
